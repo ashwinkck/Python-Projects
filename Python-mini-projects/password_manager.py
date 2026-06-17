993bf79e7d9d562166002dc841a085669c68b716
Ashwin Krishnan <ashwinkrishnanck8@gmail.com>
2026-06-17 21:37:08 +0530

    fix: decrypt passwords for viewing and ensure encrypted passwords are stored as strings

--- Python-mini-projects/password_manager.py
+++ Python-mini-projects/password_manager.py
@@ -22,7 +22,7 @@ def view():
         for line in f.readlines():
             data = line.rstrip()
             user, passw = data.split("|")
-            print("User:",user,", Password:",passw)
+            print("User:",user,", Password:",str(fer.decrypt(passw.encode())))
             
 
 def add():
@@ -30,7 +30,7 @@ def add():
     pwd = input("password: ")
 
     with open('passwords.txt', 'a') as f:
-        f.write(name + "|" + fer.encrypt(pwd.encode()) + "\n")
+        f.write(name + "|" + str(fer.encrypt(pwd.encode())) + "\n")
 
 
 while True:
