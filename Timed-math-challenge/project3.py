a30f0d5c8304fa928e823ca509758e0ff7a5fcb2
Ashwin Krishnan <ashwinkrishnanck8@gmail.com>
2026-06-24 20:56:57 +0530

    creating an input for total problem with a for loop, inside with a while and if

--- Timed-math-challenge/project3.py
+++ Timed-math-challenge/project3.py
@@ -3,6 +3,7 @@ import random
 OPERATORS = ["+","-","*"]
 MIN_OPERAND = 3
 MAX_OPERAND = 12
+TOTAL_PROBLEMS = 10
 
 def generate_problem():
     left = random.randint(MIN_OPERAND,MAX_OPERAND)
@@ -13,5 +14,10 @@ def generate_problem():
     answer = eval(expr)
     return expr, answer
 
-expr, answer = generate_problem()
-print(f"{expr} = {answer}")
+for i in range(TOTAL_PROBLEMS):
+    expr,answer  = generate_problem()
+    while True:
+        guess = input("Problem #" + str(i+1)+ ":"+expr+ " = ")
+        if guess == str(answer):
+            break
+        
