0d4e7b203ea95c88eb1a773781b02ca2eb30951b
Ashwin Krishnan <ashwinkrishnanck8@gmail.com>
2026-06-25 14:47:40 +0530

    feat: add function to get number of lines for betting

--- text-based-slot-machine-medium/main.py
+++ text-based-slot-machine-medium/main.py
@@ -1,4 +1,7 @@
+MAX_LINES = 3
 
+
+# Deposit here
 def deposit():
     while True:
         amount = input("Enter your deposit? $")
@@ -13,4 +16,27 @@ def deposit():
 
 
     return amount
+
+# getting total no of lines
+def get_number_of_lines():
+    while True:
+        lines = input(f"Enter the no of lines to bet on (1 - {MAX_LINES}) ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1<= lines <= MAX_LINES:
+                break
+            else:
+                print(f"Line must be in between 1 - {MAX_LINES}")
+        else:
+            print("Please enter a number. ")
+
+
+    return lines
+
+
+
+def main():
+    balance = deposit()
+    lines = get_number_of_lines()
     
+main()
