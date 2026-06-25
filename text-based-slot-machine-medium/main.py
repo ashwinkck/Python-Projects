21f338e9d32d2a5a9f13ba72e4c82ece94f428db
Ashwin Krishnan <ashwinkrishnanck8@gmail.com>
2026-06-25 12:38:18 +0530

    done_with_deposit()

--- text-based-slot-machine-medium/main.py
+++ text-based-slot-machine-medium/main.py
@@ -1,1 +1,16 @@
-print("ash")
+
+def deposit():
+    while True:
+        amount = input("Enter your deposit? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Amount must be greater than 0")
+        else:
+            print("Please enter a number. ")
+
+
+    return amount
+    
