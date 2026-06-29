d80f0095d76528d6e463f0437c07c4c9493a7636
Ashwin Krishnan <ashwinkrishnanck8@gmail.com>
2026-06-29 15:41:05 +0530

    Added a new function

--- WPM-typing/tutorial.py
+++ WPM-typing/tutorial.py
@@ -1,14 +1,20 @@
 import curses #helping with styling of the terminal
 from curses import wrapper
 
+def start_screen(stdscr):
+    stdscr.clear() # Clearing the screen
+    stdscr.addstr("Welcome to the speed Typing Test!")
+    stdscr.addstr("\nPress any key to begin!") # adding text in to the scrreen
+    stdscr.refresh()
+    stdscr.getkey()
+
+
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK) # adding styling with foreground green and background white with an id 1
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)# Same shi with a second id
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
-    stdscr.clear() # Clearing the screen
-    stdscr.addstr(0 0, "Hello World!", curses.color_pair(2)) # adding text in to the scrreen
-    stdscr.refresh()# Refreshing the screen
-    stdscr.getkey() # The covering in which of the o/p when we are putting the values
+    start_screen(stdscr)
+    
 
 wrapper(main)
