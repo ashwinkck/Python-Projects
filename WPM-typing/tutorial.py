cba9334a2c9976164a53df0356707565764b0434
Ashwin Krishnan <ashwinkrishnanck8@gmail.com>
2026-06-29 15:00:43 +0530

    changed the bg of the curses because im using electricity(omarchy default terminal)

--- WPM-typing/tutorial.py
+++ WPM-typing/tutorial.py
@@ -2,8 +2,8 @@ import curses #helping with styling of the terminal
 from curses import wrapper
 
 def main(stdscr):
-    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_WHITE) # adding styling with foreground green and background white with an id 1
-    curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_WHITE)# Same shi with a second id
+    curses.init_pair(1, curses.COLOR_GREEN, -1) # adding styling with foreground green and background white with an id 1
+    curses.init_pair(2, curses.COLOR_YELLOW, -1)# Same shi with a second id
     stdscr.clear() # Clearing the screen
     stdscr.addstr("Hello World!", curses.color_pair(1)) # adding text in to the scrreen
     stdscr.refresh()# Refreshing the screen
