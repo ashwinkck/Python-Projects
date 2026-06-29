556ad193f6c01bc5f6c2a44c62f094bad0fda0a3
Ashwin Krishnan <ashwinkrishnanck8@gmail.com>
2026-06-29 16:13:32 +0530

    feat: add wpm_test function to display target text

--- WPM-typing/tutorial.py
+++ WPM-typing/tutorial.py
@@ -9,12 +9,22 @@ def start_screen(stdscr):
     stdscr.getkey()
 
 
+def wpm_test(stdscr):
+    target_text = "Hello world this is some test text for this app!"
+    current_text = []
+    stdscr.clear() # Clearing the screen
+    stdscr.addstr(target_text)
+    stdscr.refresh()
+    stdscr.getkey()
+
+
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK) # adding styling with foreground green and background white with an id 1
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)# Same shi with a second id
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
     start_screen(stdscr)
+    wpm_test(stdscr)
     
 
 wrapper(main)
