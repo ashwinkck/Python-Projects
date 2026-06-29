62a6f5ac88c16986b25743f65f3ff0eab29b6d30
Ashwin Krishnan <ashwinkrishnanck8@gmail.com>
2026-06-29 14:49:09 +0530

    added a small part of a function of standard screen

--- WPM-typing/tutorial.py
+++ WPM-typing/tutorial.py
@@ -1,1 +1,10 @@
 import curses #helping with styling of the terminal
+from curses import wrapper
+
+def main(stdscr):
+    stdscr.clear() # Clearing the screen
+    stdscr.addstr("Hello World!") # adding text in to the scrreen
+    stdscr.refresh()# Refreshing the screen
+    stdscr.getkey() # The covering in which of the o/p when we are putting the values
+
+wrapper(main)
