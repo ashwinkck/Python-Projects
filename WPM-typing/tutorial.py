34726e4ac3186a32969c9c4da815aa458755c22f
Ashwin Krishnan <ashwinkrishnanck8@gmail.com>
2026-06-30 13:38:29 +0530

    update

--- WPM-typing/tutorial.py
+++ WPM-typing/tutorial.py
@@ -32,6 +32,7 @@ def wpm_test(stdscr):
         stdscr.refresh()
 
 
+
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK) # adding styling with foreground green and background white with an id 1
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)# Same shi with a second id
