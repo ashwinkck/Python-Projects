161435c173e2daf26b15d78c21199d75cc53c6fe
Ashwin Krishnan <ashwinkrishnanck8@gmail.com>
2026-07-01 21:02:04 +0530

    changed the pm name for better understanding

--- alarm-clock/alarm_clock.py
+++ alarm-clock/alarm_clock.py
@@ -4,16 +4,20 @@ import time
 CLEAR = "\033[2J" # To clear the terminal
 CLEAR_AND_RETURN = "\033[H" #will clear and do the maths in a single line
 
-def alarm(seconds, ring_count):
+minutes = int(input("How many minutes to wait: "))
+seconds = int(input("How many seconds to wait: "))
+ring = int(input("How many times do you want it to repeat it: "))
+total_seconds = minutes * 60 + seconds
+def alarm(t_seconds, ring_count):
     time_elapsed = 0
     
     print(CLEAR)
-    while time_elapsed < seconds:
+    while time_elapsed < t_seconds:
         time.sleep(1)
         time_elapsed += 1
 
 
-        time_left = seconds - time_elapsed
+        time_left = t_seconds - time_elapsed
         minutes_left = time_left // 60
         seconds_left = time_left % 60
 
@@ -22,8 +26,4 @@ def alarm(seconds, ring_count):
         playsound("alarm.mp3")
         time.sleep(1)
 
-minutes = int(input("How many minutes to wait: "))
-seconds = int(input("How many seconds to wait: "))
-ring = int(input("How many times do you want it to repeat it: "))
-total_seconds = minutes * 60 + seconds
 alarm(total_seconds, ring)
